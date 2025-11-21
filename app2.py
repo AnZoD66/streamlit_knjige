a352971f89cd09a62829d2d17f75f2966dcf028a
AnZoD66 <antoivandic04@gmail.com>
2025-11-21 20:45:40 +0100

    Update app2.py

--- app2.py
+++ app2.py
@@ -46,8 +46,8 @@ if st.button("Dodajte knjigu"):
 st.subheader("Pretražite knjige")
 filtrirani = df.copy()
 
-autor_filt = st.text_input("Pretražite po autoru", key="autor_input")
-zanr_filt = st.text_input("Pretražite po žanru", key="zanr_input")
+autor_filt = st.text_input("Pretražite po autoru")
+zanr_filt = st.text_input("Pretražite po žanru")
 godina_filt = st.number_input("Pretražite po godini", step=1, format="%d")
 
 if autor_filt:
@@ -63,7 +63,7 @@ st.dataframe(filtrirani)
 
 st.subheader("Brisanje knjiga")
 
-knjige_opcije = df.apply(lambda row: f"{row["Naslov"]}-{row["Autor"]} ({row["Godina"]})", axis=1).tolist()
+knjige_opcije = df.apply(lambda row: f"{row["Naslov"]}-{row["Autor"]} ({row["Godina"]})", axis= 1).tolist()
 knjige_za_brisanje = st.selectbox("Odaberite knjigu za brisanje", options=knjige_opcije)
 
 if st.button("Obrišite knjigu"):
@@ -77,6 +77,7 @@ st.subheader("Top 5 knjiga")
 
 top5 = df.sort_values(by = "Ocjena", ascending=False).head(5)
 st.table(top5)
+
 
 
 
